6498d9f3dac504f6aaf2ce6be8531e0e7dfaea0c
Allee Zarrini <azarrini@Allees-MBP-2.hsd1.co.comcast.net>
2019-11-30 12:14:45 -0700

    Completed implementation of upgma algorithm

--- upgma.cpp
+++ upgma.cpp
@@ -1,17 +1,26 @@
 #include<iostream>
 #include<fstream>
 #include<random>
+#include<vector>
 
 using namespace std;
 
+#define INF 100000000
+
 void readClusters(string filename);
 void generateCluster(int n, int max, int seed);
-void printCluster();
+void buildTree();
+void print2dVector(vector<vector<double> >& v);
+string add2Map(string a, string b);
+string getString(char a); 
+void printTree(); 
 
 int n;
-double **C;
+vector<vector<double> > C;
+vector<string> tree;
 
 int main(int argc, char* argv[]) {
+
 	if (argc != 3 && argc != 5)
 	{
 		std::cerr << "Arguments must be presented in either of the following ways." << std::endl;
@@ -30,8 +39,10 @@ int main(int argc, char* argv[]) {
 		int seed = atoi(argv[4]);
 		generateCluster(n, max, seed);
 	}
-
-	printCluster();
+    
+    print2dVector(C);
+    buildTree();
+    printTree();
 
 	return 0;
 }
@@ -45,12 +56,18 @@ void readClusters(string filename) {
 
 	fin >> n;
 
-	C = new double*[n];
+	vector<vector<double> > t(
+        n,
+        vector<double>(n));
+    C = t;
+
+    tree.reserve(2*n-1);
 
 	for (int i = 0; i < n; i++) {
-		C[i] = new double[n];
-		for(int j = 0; j < n; j++) {
-			fin >> C[i][j];
+        tree.push_back(getString((char) ('a'+i)));
+		for(int j = i + 1; j < n; j++) {
+            fin >> C[i][j];
+			C[j][i] = C[i][j];
 		}
 	}
 
@@ -60,21 +77,80 @@ void readClusters(string filename) {
 void generateCluster(int n, int max, int seed) {
 	srand(seed);
 
-	C = new double*[n];
+	vector<vector<double> > t(
+        n,
+        vector<double>(n));
+    C = t;
+
+    
+    tree.reserve(2*n-1);
 
 	for (int i = 0; i < n; i++) {
-		C[i] = new double[n];
-		for(int j = 0; j < n; j++) {
+        tree.push_back(getString((char) ('a'+i)));
+        for(int j = i + 1; j < n; j++) {
 			C[i][j] = double(rand()) / RAND_MAX * max;
+			C[j][i] = C[i][j];
 		}
 	}
 }
 
-void printCluster() {
-	for (int i = 0; i < n; i++) {
-		for(int j = 0; j < n; j++) {
-			cout << C[i][j] << "\t";
+void buildTree() {
+    static int cnt = 0;
+    if (C.size() == 1) {
+        return;
+    }
+
+    double min = INF;
+    int l, r;
+
+ 	for (int i = 0; i < C.size(); i++) {
+        for(int j = i + 1; j < C[i].size(); j++) {
+	        if (min > C[i][j]) {
+                min = C[i][j];
+                l = i;
+                r = j;
+            }
+        }
+	}
+    
+    for (int i = 0; i < C.size(); i++) {
+        if (i == l || i == r) continue;
+            
+        double dlr = (C[i][l] + C[i][r]) / 2;
+        C[i][l] = dlr;
+        C[l][i] = dlr;
+
+        C[i].erase(C[i].begin()+r);
+    }
+    C[l].erase(C[l].begin()+r);
+    C.erase(C.begin()+r);
+   
+    tree[l] = add2Map(tree[l], tree[r]);
+    tree.erase(tree.begin()+r);
+
+    buildTree();
+}
+
+void print2dVector(vector<vector<double> >& v) {
+    cout << v.size() << endl;
+	for (int i = 0; i < v.size(); i++) {
+		for(int j = 0; j < v[i].size(); j++) {
+			cout << fixed << setprecision(4) << v[i][j] << "\t";
 		}
 		cout << endl;
 	}
+    cout << endl;
+}
+
+string add2Map(string a, string b) {
+    return "( " + a + " , " + b + " )"; 
+}
+
+string getString(char a) {
+    string t(1,a);
+    return t;
+}
+
+void printTree() {
+   cout << tree[0] << endl;
 }
